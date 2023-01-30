f704d39ff5087d72e9558976bd78f2f95eb7908e
me <aoffis.ntp@gmail.com>
2023-01-31 00:22:01 +0700

    restore from backup

---
+++ components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="container cl-ci-primary p-4">
+			<div className="sp h-2" style={{ background: "var(--ci-primary)" }}></div>
+			<div className="h-52">
+				<div className="h-full flex flex-col items-start justify-center">
+					<h1 className="">.todaynews</h1>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
